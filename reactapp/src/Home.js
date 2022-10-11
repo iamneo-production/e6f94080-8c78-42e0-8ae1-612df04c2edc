2e5ec8f41bb96f8e290e7257f308aa524e5cac07
Nandhini323 <nandhiniarulmary@gmail.com>
2022-10-11 08:15:54 +0000

    FRONTEND

---
+++ reactapp/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      Welcome to the Home Page!
+      <div className='text-center'><a style={{textDecoration: 'none'}} href='login1'>Login</a></div>
+    </div>
+  );
+}
+
+export default Home;
